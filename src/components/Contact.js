873b320c31231cadbbf568c7c5713376bbc3bd86
aldhielman <rivaldithoriq@gmail.com>
2022-06-19 21:22:55 +0700

    finish challenge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  const { name, phone, email, photo } = data;
+  return (
+    <div className="card">
+      <img src={photo} className="contact-photo" alt="pict" />
+      <div className="contact-detail">
+        <h3>{name}</h3>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <h1>Call a Friend</h1>
+      <p>your friendly contact app</p>
+      <p className="star">
+        *******************************************************************************************
+      </p>
+    </>
+  );
+};
 
 export default Header;
